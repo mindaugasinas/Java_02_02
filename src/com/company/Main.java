b05b62624b383c395a2725185370e3e062105f2a
mindaugasinas <sinkeviciusm@gmail.com>
2018-01-30 14:58:07 +0200

    Antra pamoka

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Iveskite zodi");
+
+        String Zodis = sc.nextLine();
+
+        System.out.println("Zodyje yra " + Zodis.length() + " raides");
+
 	// write your code here
     }
 }
